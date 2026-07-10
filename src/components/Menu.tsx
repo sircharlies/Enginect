3316200201f197acf693bfcdd647fb6d2da14056
Carlos Barreto <carlos.barreto@evertecinc.com.br>
2026-07-10 17:43:20 -0300

    Layout upgrade

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 
 const simulations = [
+  {title: "🏠 Home",           path: "/"},
   {title: "⚪ Bolas",           path: "/balls"},
   {title: "✨ Partículas",      path: "/particles"},
   {title: "🏖️ Areia",           path: "/sand"},
@@ -13,12 +14,12 @@ export default function Menu() {
   return (
     <div className="p-4">
         <h2 className="pb-4">Simulações</h2>
-        <div className="grid">
+        <div className="grid text-start">
             {simulations.map((simulation) => (
             <Link
                 key={simulation.path}
                 to={simulation.path}
-                className="card" 
+                className="card duration-300 hover:translate-x-2" 
             >
                 {simulation.title}
             </Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
-import Menu from "../components/Menu";
-
 export default function Home() {
   return (
     <div className="home">
       <h1>Simulation Lab</h1>
-      <p>Escolha uma simulação.</p>
+      <p>Escolha uma simulação ao lado.</p>
     </div>
   );
 }
